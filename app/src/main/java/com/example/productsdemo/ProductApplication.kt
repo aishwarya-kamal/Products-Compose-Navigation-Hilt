a4786ae8dbeb86a16e5287b7cd0fc822cfec5a6d
JoseMarrima <benderspor@gmail.com>
2023-09-27 15:59:32 +0200

    Added utils and ProductApplication

---
+++ app/src/main/java/com/example/productsdemo/ProductApplication.kt
@@ -0,0 +1,7 @@
+package com.example.productsdemo
+
+import android.app.Application
+
+
+class ProductApplication: Application() {
+}
---
+++ app/src/main/java/com/example/productsdemo/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.productsdemo.utils
+
+
+const val BASE_URL = "http://api.themoviedb.org/3/"
+const val API_KEY = "328c283cd27bd1877d9080ccb1604c91"
